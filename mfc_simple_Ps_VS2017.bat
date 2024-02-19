7880b3cc22bbc5ce93cd9869602aab828d9105bc
Toshiaki Kawabata <toshiaki@techsoft3d.com>
2024-02-19 16:05:46 +0900

    Ported 3DF_2900 and Ps34.1

--- mfc_simple_Ps_VS2017.bat
+++
@@ -1,6 +0,0 @@
-SET HVISUALIZE_INSTALL_DIR=C:\SDK\Visualize\HOOPS_3DF_2710
-SET PARASOLID_INSTALL_DIR=C:\SDK\Parasolid\v32.0\kernel
-SET P_SCHEMA=C:\SDK\Parasolid\v32.0\kernel\base64_vc14\schema
-
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" mfc_simple_Ps_v141.sln
-
---
+++ mfc_simple_Ps_VS2019.bat
@@ -0,0 +1,6 @@
+SET HVISUALIZE_INSTALL_DIR=C:\SDK\Visualize\HOOPS_3DF_2900
+SET PARASOLID_INSTALL_DIR=C:\SDK\Parasolid\v34.1\kernel
+SET P_SCHEMA=C:\SDK\Parasolid\v34.1\kernel\base64_vc14\schema
+
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" mfc_simple_Ps_v142.sln
+
